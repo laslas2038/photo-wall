70da0b70bcf934ec6345ad3187cade519851807a
laslas2038 <laslas2038@gmail.com>
2026-03-25 17:46:21 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 export const CLOUD_NAME = "dcdur6xo9";
 export const UPLOAD_PRESET = "photowall";
-export const ADMIN_PASSWORD = "lascity2000";
+export const FOLDER = "photo-wall";
 export const TIMER = 3000;
