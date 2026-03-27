9d19a286f1abad3ec4208826ab840e5e09ceaccd
laslas2038 <laslas2038@gmail.com>
2026-03-27 15:13:34 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,19 @@
+// 🔥 CLOUDINARY
 export const CLOUD_NAME = "dcdur6xo9";
 export const UPLOAD_PRESET = "photowall";
+
+// 🔥 EVENTO
+export const EVENTO = "LAS";
+
+// 🔥 TIMER
 export const TIMER = 3000;
+
+// 🔥 FIREBASE
+export const firebaseConfig = {
+  apiKey: "AIzaSyDv_rmS7ymNfw1x-nBvfA7j9H0daHfz74U",
+  authDomain: "photo-wall-debce.firebaseapp.com",
+  projectId: "photo-wall-debce",
+  storageBucket: "photo-wall-debce.firebasestorage.app",
+  messagingSenderId: "437062349484",
+  appId: "1:437062349484:web:fb1e0daf8b32d0cc8ce509"
+};
